6f8277e949d224a6dc94e9c8696324650195ea3e
Rusty Lynch <rusty.lynch@intel.com>
2012-12-17 18:07:32 -0800

    Adding elm_finger_size_get and elm_finger_size_set
    Adding the previously removed elm_finger_size_[get|set] functions
    to work past breakage in the Tizen 2.0 alpha middleware

--- src/lib/elm_config.c
+++ src/lib/elm_config.c
@@ -2607,6 +2607,12 @@ elm_config_finger_size_get(void)
 }
 
 EAPI void
+elm_finger_size_set(Evas_Coord size)
+{
+   elm_config_finger_size_set(size);
+}
+
+EAPI void
 elm_config_finger_size_set(Evas_Coord size)
 {
    if (size < 0) return;
@@ -3514,3 +3520,10 @@ _elm_config_shutdown(void)
    if (_elm_key_bindings)
      eina_hash_free(_elm_key_bindings);
 }
+
+EAPI Evas_Coord
+elm_finger_size_get(void)
+{
+   return elm_config_finger_size_get();
+}
+
