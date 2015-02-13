tasn/elementary

gengrid: select the focused item on enter key input
there was no way to select an item with key input on ELM_ITEM_SELECT_ON_FOCUS_DISABLE mode.
this patch enables select and multi select with enter/space key input.

@fix

src/lib/elm_gengrid.c
@@ -2961,19 +2961,53 @@ _key_action_move(Evas_Object *obj, const char *params)
 }
 
 static Eina_Bool
-_key_action_select(Evas_Object *obj, const char *params EINA_UNUSED)
+_key_action_select(Evas_Object *obj, const char *params)
 {
    ELM_GENGRID_DATA_GET(obj, sd);
-   Elm_Object_Item *eo_it = NULL;
+   Elm_Object_Item *eo_it = elm_object_focused_item_get(obj);
+   ELM_GENGRID_ITEM_DATA_GET(eo_it, it);
+
+   if (sd->multi &&
+       ((sd->multi_select_mode != ELM_OBJECT_MULTI_SELECT_MODE_WITH_CONTROL) ||
+        (!strcmp(params, "multi"))))
+     {
+        if (!it->selected)
+          {
+             it->highlight_cb(it);
+             it->sel_cb(it);
+          }
+        else it->unsel_cb(it);
+     }
+   else
+     {
+        if (!it->selected)
+          {
+             while (sd->selected)
+               {
+                  Elm_Object_Item *eo_sel = sd->selected->data;
+                  Elm_Gen_Item *sel = eo_data_scope_get(eo_sel, ELM_GENGRID_ITEM_CLASS);
+                  it->unsel_cb(sel);
+               }
+          }
+        else
+          {
+             const Eina_List *l, *l_next;
+             Elm_Object_Item *eo_item2;
+
+             EINA_LIST_FOREACH_SAFE(sd->selected, l, l_next, eo_item2)
+               {
+                  ELM_GENGRID_ITEM_DATA_GET(eo_item2, item2);
+                  if (item2 != it) it->unsel_cb(item2);
+               }
+          }
+        it->highlight_cb(it);
+        it->sel_cb(it);
+     }
 
    if (!sd->multi)
-     {
-        eo_it = elm_object_focused_item_get(obj);
-        ELM_GENGRID_ITEM_DATA_GET(eo_it, it);
-        evas_object_smart_callback_call(WIDGET(it), SIG_ACTIVATED, eo_it);
-        return EINA_TRUE;
-     }
-   return EINA_FALSE;
+     evas_object_smart_callback_call(WIDGET(it), SIG_ACTIVATED, eo_it);
+
+   return EINA_TRUE;
 }
 
 static Eina_Bool
@@ -4941,6 +4975,7 @@ _elm_gengrid_elm_interface_atspi_widget_action_elm_actions_get(Eo *obj EINA_UNUS
           { "move,first", "move", "first", _key_action_move},
           { "move,last", "move", "last", _key_action_move},
           { "select", "select", NULL, _key_action_select},
+          { "select,multi", "select", "multi", _key_action_select},
           { "escape", "escape", NULL, _key_action_escape},
           { NULL, NULL, NULL, NULL }
    };

tizenorg/platform.upstream.elementary

Conflicts:
	src/lib/elm_gengrid.c
Change-Id: I9071395da5128e5749ca2fb51f71c063f6faa641

src/lib/elm_gengrid.c
@@ -2879,18 +2879,46 @@ _key_action_move(Evas_Object *obj, const char *params)
 }
 
 static Eina_Bool
-_key_action_select(Evas_Object *obj, const char *params EINA_UNUSED)
+_key_action_select(Evas_Object *obj, const char *params)
 {
    ELM_GENGRID_DATA_GET(obj, sd);
-   Elm_Object_Item *it = NULL;
+   Elm_Object_Item *item = elm_object_focused_item_get(obj);
+   Elm_Gen_Item *it = (Elm_Gen_Item *)item;
+
+   if (sd->multi &&
+       ((sd->multi_select_mode != ELM_OBJECT_MULTI_SELECT_MODE_WITH_CONTROL) ||
+        (!strcmp(params, "multi"))))
+     {
+        if (!it->selected)
+          {
+             it->highlight_cb(it);
+             it->sel_cb(it);
+          }
+        else it->unsel_cb(it);
+     }
+   else
+     {
+        if (!it->selected)
+          {
+             while (sd->selected)
+               it->unsel_cb(sd->selected->data);
+          }
+        else
+          {
+             const Eina_List *l, *l_next;
+             Elm_Gen_Item *it2;
+
+             EINA_LIST_FOREACH_SAFE(sd->selected, l, l_next, it2)
+               if (it2 != it) it->unsel_cb(it2);
+          }
+        it->highlight_cb(it);
+        it->sel_cb(it);
+     }
 
    if (!sd->multi)
-     {
-        it = elm_object_focused_item_get(obj);
-        evas_object_smart_callback_call(WIDGET(it), SIG_ACTIVATED, it);
-        return EINA_TRUE;
-     }
-   return EINA_FALSE;
+     evas_object_smart_callback_call(WIDGET(it), SIG_ACTIVATED, it);
+
+   return EINA_TRUE;
 }
 
 static Eina_Bool
@@ -4814,6 +4842,7 @@ _elm_gengrid_elm_interface_atspi_widget_action_elm_actions_get(Eo *obj EINA_UNUS
           { "move,first", "move", "first", _key_action_move},
           { "move,last", "move", "last", _key_action_move},
           { "select", "select", NULL, _key_action_select},
+          { "select,multi", "select", "multi", _key_action_select},
           { "escape", "escape", NULL, _key_action_escape},
           { NULL, NULL, NULL, NULL }
    };