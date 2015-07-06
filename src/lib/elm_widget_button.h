7cdf62184bf94af8ed692bfcce0a15f939e18501
SangHyeon Lee <sh10233.lee@samsung.com>
2015-07-06 17:56:46 +0900

    button : add longpress callback
    The Button need longpress callback for Product TV feature
    when they get pressed long enough longpress timeout.
    
    Change-Id: I12b42981350f6e9dc18e1edfe57505f26dce0d9d
    Signed-off-by: SangHyeon Lee <sh10233.lee@samsung.com>
    Signed-off-by: jinwoo.shin <jw0227.shin@samsung.com>

--- src/lib/elm_widget_button.h
+++ src/lib/elm_widget_button.h
@@ -34,6 +34,9 @@ typedef struct _Elm_Button_Data
 
    Eina_Bool             autorepeat : 1; /**< Whether auto-repetition of clicks is enabled or not (bound to _Elm_Button_Smart_Class::admits_autorepeat) */
    Eina_Bool             repeating : 1; /**< Whether auto-repetition is going on */
+   // TIZEN_ONLY(20150706) : add longpress callback
+   Ecore_Timer           *longpress_timer;
+   // END-ONLY
 } Elm_Button_Data;
 
 /**
