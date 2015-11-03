f4a0815b02ac244ea72b6fd0d772462bf5b9d9df
Youngbok Shin <youngb.shin@samsung.com>
2015-11-03 13:53:05 +0900

    Resolve the conflicts for elm_focus
    Change-Id: I8b1e333b5a98925c0e97fb6f7015ee421c7d9da8

--- src/bin/test_focus_policy.c
+++ src/bin/test_focus_policy.c
@@ -14,17 +14,6 @@ _rdg_changed_cb(void *data, Evas_Object *obj,
    switch (value)
      {
       case 0:
-<<<<<<< HEAD
-        elm_object_text_set(bt, "Test Button(MOUSE CLICK or KEY)");
-        elm_object_focus_move_policy_set(bt, ELM_FOCUS_MOVE_POLICY_CLICK);
-        break;
-      case 1:
-        elm_object_text_set(bt, "Test Button(MOUSE IN or KEY)");
-        elm_object_focus_move_policy_set(bt, ELM_FOCUS_MOVE_POLICY_IN);
-        break;
-      case 2:
-        elm_object_text_set(bt, "Test Button(KEY ONLY)");
-=======
         elm_object_text_set(bt, "Test Button (MOUSE CLICK or KEY)");
         elm_object_focus_move_policy_set(bt, ELM_FOCUS_MOVE_POLICY_CLICK);
         break;
@@ -34,7 +23,6 @@ _rdg_changed_cb(void *data, Evas_Object *obj,
         break;
       case 2:
         elm_object_text_set(bt, "Test Button (KEY ONLY)");
->>>>>>> opensource/master
         elm_object_focus_move_policy_set(bt, ELM_FOCUS_MOVE_POLICY_KEY_ONLY);
         break;
       default:
@@ -45,11 +33,7 @@ _rdg_changed_cb(void *data, Evas_Object *obj,
 void
 test_focus_object_policy(void *data EINA_UNUSED, Evas_Object *obj EINA_UNUSED, void *event_info EINA_UNUSED)
 {
-<<<<<<< HEAD
-   Evas_Object *win, *fr, *bx, *bt, *test_bt, *rdg, *rd;
-=======
    Evas_Object *win, *fr, *fr2, *bx, *fr_bx, *bt, *test_bt, *lbl, *rdg, *rd;
->>>>>>> opensource/master
 
    win = elm_win_util_standard_add("focus-object-policy", "Focus Object Policy");
    elm_win_autodel_set(win, EINA_TRUE);
@@ -67,66 +51,6 @@ test_focus_object_policy(void *data EINA_UNUSED, Evas_Object *obj EINA_UNUSED, v
    elm_object_content_set(fr, bx);
    evas_object_show(bx);
 
-<<<<<<< HEAD
-   bt = elm_button_add(bx);
-   elm_object_text_set(bt, "Button 1");
-   evas_object_size_hint_weight_set(bt, EVAS_HINT_EXPAND, EVAS_HINT_EXPAND);
-   evas_object_size_hint_align_set(bt, EVAS_HINT_FILL, EVAS_HINT_FILL);
-   elm_box_pack_end(bx, bt);
-   evas_object_show(bt);
-
-   bt = elm_button_add(bx);
-   elm_object_text_set(bt, "Button 2");
-   evas_object_size_hint_weight_set(bt, EVAS_HINT_EXPAND, EVAS_HINT_EXPAND);
-   evas_object_size_hint_align_set(bt, EVAS_HINT_FILL, EVAS_HINT_FILL);
-   elm_box_pack_end(bx, bt);
-   evas_object_show(bt);
-
-   // a button to test focus object policy
-   test_bt = elm_button_add(bx);
-   elm_object_text_set(test_bt, "Test Button(MOUSE CLICK or KEY)");
-   evas_object_size_hint_weight_set(test_bt, EVAS_HINT_EXPAND, EVAS_HINT_EXPAND);
-   evas_object_size_hint_align_set(test_bt, EVAS_HINT_FILL, EVAS_HINT_FILL);
-   elm_box_pack_end(bx, test_bt);
-   evas_object_show(test_bt);
-   elm_object_focus_move_policy_set(bt, ELM_FOCUS_MOVE_POLICY_CLICK);
-
-   bt = elm_button_add(bx);
-   elm_object_text_set(bt, "Button 4");
-   evas_object_size_hint_weight_set(bt, EVAS_HINT_EXPAND, EVAS_HINT_EXPAND);
-   evas_object_size_hint_align_set(bt, EVAS_HINT_FILL, EVAS_HINT_FILL);
-   elm_box_pack_end(bx, bt);
-   evas_object_show(bt);
-
-   // radios to select focus object policy
-   rd = elm_radio_add(bx);
-   elm_radio_state_value_set(rd, 0);
-   evas_object_size_hint_align_set(rd, 0.0, 0.5);
-   elm_object_text_set(rd, "Focus Move Pollicy Mouse Click");
-   elm_box_pack_end(bx, rd);
-   evas_object_show(rd);
-   evas_object_smart_callback_add(rd, "changed", _rdg_changed_cb, test_bt);
-
-   rdg = rd;
-
-   rd = elm_radio_add(bx);
-   elm_radio_state_value_set(rd, 1);
-   elm_radio_group_add(rd, rdg);
-   evas_object_size_hint_align_set(rd, 0.0, 0.5);
-   elm_object_text_set(rd, "Focus Move Policy Mouse In");
-   elm_box_pack_end(bx, rd);
-   evas_object_show(rd);
-   evas_object_smart_callback_add(rd, "changed", _rdg_changed_cb, test_bt);
-
-   rd = elm_radio_add(bx);
-   elm_radio_state_value_set(rd, 2);
-   elm_radio_group_add(rd, rdg);
-   evas_object_size_hint_align_set(rd, 0.0, 0.5);
-   elm_object_text_set(rd, "Focus Move Pollicy Key Only");
-   elm_box_pack_end(bx, rd);
-   evas_object_show(rd);
-   evas_object_smart_callback_add(rd, "changed", _rdg_changed_cb, test_bt);
-=======
    /* frame 1 for normal buttons */
      {
         fr2 = elm_frame_add(bx);
@@ -220,7 +144,6 @@ test_focus_object_policy(void *data EINA_UNUSED, Evas_Object *obj EINA_UNUSED, v
         evas_object_show(rd);
         evas_object_smart_callback_add(rd, "changed", _rdg_changed_cb, test_bt);
      }
->>>>>>> opensource/master
 
    evas_object_resize(win, 320, 320);
    evas_object_show(win);
--- src/lib/elm_focus.h
+++ src/lib/elm_focus.h
@@ -40,27 +40,6 @@
  */
 typedef enum
 {
-<<<<<<< HEAD
-   ELM_FOCUS_PREVIOUS, /**< previous direction */
-   ELM_FOCUS_NEXT,     /**< next direction */
-   ELM_FOCUS_UP,       /**< up direction */
-   ELM_FOCUS_DOWN,     /**< down direction */
-   ELM_FOCUS_RIGHT,    /**< right direction */
-   ELM_FOCUS_LEFT,      /**< left direction */
-} Elm_Focus_Direction;
-=======
-   ELM_FOCUS_REGION_SHOW_WIDGET, /**< as a widget */
-   ELM_FOCUS_REGION_SHOW_ITEM, /**< as an item */
-} Elm_Focus_Region_Show_Mode;
->>>>>>> opensource/master
-
-/**
- * Focus region show mode.
- *
- * @ingroup Focus
- */
-typedef enum
-{
    ELM_FOCUS_REGION_SHOW_WIDGET, /**< as a widget */
    ELM_FOCUS_REGION_SHOW_ITEM, /**< as an item */
 } Elm_Focus_Region_Show_Mode;
@@ -354,11 +333,7 @@ EAPI void                 elm_object_focus_move_policy_set(Evas_Object *obj, Elm
  * @param obj The Elementary widget to get the information from
  * @return The focus movement policy
  *
-<<<<<<< HEAD
- * Get how the focus is moved to the give Elemenray object. It can be
-=======
  * Get how the focus is moved to the give Elementary object. It can be
->>>>>>> opensource/master
  * #ELM_FOCUS_MOVE_POLICY_CLICK, #ELM_FOCUS_MOVE_POLICY_IN,
  * or #ELM_FOCUS_MOVE_POLICY_KEY_ONLY.
  * The first means elementary focus is moved on elementary object click.
@@ -372,11 +347,7 @@ EAPI void                 elm_object_focus_move_policy_set(Evas_Object *obj, Elm
  *
  * @ingroup Focus
  */
-<<<<<<< HEAD
-EAPI Elm_Focus_Move_Policy  elm_object_focus_move_policy_get(Evas_Object *obj);
-=======
 EAPI Elm_Focus_Move_Policy  elm_object_focus_move_policy_get(const Evas_Object *obj);
->>>>>>> opensource/master
 
 /**
  * Set the focus region show mode to a given Elementary object.
