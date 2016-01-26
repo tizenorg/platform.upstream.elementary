ef1edd4584dbb87ae9a6320573aa535775d25836
Bartlomiej Uliasz <b.uliasz@samsung.com>
2016-01-26 11:25:43 +0100

    Accessible roles adaptation
    Change-Id: I1c10b3f10c17eb60d6bf1d8193dc59f8aa6e83dc
    Signed-off-by: Bartlomiej Uliasz <b.uliasz@samsung.com>

--- src/lib/elm_hover.c
+++ src/lib/elm_hover.c
@@ -685,7 +685,7 @@ _elm_hover_eo_base_constructor(Eo *obj, Elm_Hover_Data *_pd EINA_UNUSED)
    eo_do(obj,
          evas_obj_type_set(MY_CLASS_NAME_LEGACY),
          evas_obj_smart_callbacks_descriptions_set(_smart_callbacks),
-         elm_interface_atspi_accessible_role_set(ELM_ATSPI_ROLE_GLASS_PANE));
+         elm_interface_atspi_accessible_role_set(ELM_ATSPI_ROLE_POPUP_MENU));
 
    return obj;
 }
