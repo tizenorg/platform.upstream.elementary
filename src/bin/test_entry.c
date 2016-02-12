b6ede027b71601ce3e9cf819544275f6f3ab6ee5
Youngbok Shin <youngb.shin@samsung.com>
2016-02-12 20:18:05 +0900

    entry: add elm_entry_select_region_get() API
    Summary:
    Already, there is a way to set a selection region:
    elm_entry_select_region_set()
    The get() API also useful and there is needs for this
    inside of elm_entry.c. Add the API and replace codes
    in atspi_text_selection_get with the API.
    @feature
    
    Test Plan:
    1. Run "elementary_test -to entry3"
    2. Make a selection on text.
    3. Press "Sel" button.
    
    Reviewers: tasn, herdsman, cedric, woohyun, Jaehyun, Hermet
    
    Subscribers: Hermet
    
    Differential Revision: https://phab.enlightenment.org/D3639
    
    Change-Id: I90a098226f06db3dad9559e9d61f3e3b7a30752b

--- src/bin/test_entry.c
+++ src/bin/test_entry.c
@@ -35,6 +35,9 @@ my_entry_bt_3(void *data, Evas_Object *obj EINA_UNUSED, void *event_info EINA_UN
 {
    Evas_Object *en = data;
    const char *s = elm_entry_selection_get(en);
+   int start = 0, end = 0;
+   elm_entry_select_region_get(en, &start, &end);
+   printf("SELECTION REGION: %d - %d\n", start, end);
    printf("SELECTION:\n");
    if (s) printf("%s\n", s);
    printf("SELECTION PLAIN UTF8:\n");
@@ -241,6 +244,9 @@ my_scrolled_entry_bt_3(void *data, Evas_Object *obj EINA_UNUSED, void *event_inf
 {
    Evas_Object *en = data;
    const char *s = elm_entry_selection_get(en);
+   int start = 0, end = 0;
+   elm_entry_select_region_get(en, &start, &end);
+   printf("SELECTION REGION: %d - %d\n", start, end);
    printf("SELECTION:\n");
    if (s) printf("%s\n", s);
    printf("SELECTION PLAIN UTF8:\n");
@@ -670,6 +676,9 @@ my_ent_bt_sel(void *data, Evas_Object *obj EINA_UNUSED, void *event_info EINA_UN
 {
    Evas_Object *en = data;
    const char *s = elm_entry_selection_get(en);
+   int start = 0, end = 0;
+   elm_entry_select_region_get(en, &start, &end);
+   printf("SELECTION REGION: %d - %d\n", start, end);
    printf("SELECTION:\n");
    if (s) printf("%s\n", s);
    printf("SELECTION PLAIN UTF8:\n");
