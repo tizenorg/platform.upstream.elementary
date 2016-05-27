8017dc2904c70c4c1fb871cba8db7f7108319a68
Jiwon Kim <jiwon177.kim@samsung.com>
2016-05-27 13:46:31 +0900

    Elm entry: fix returning wrong focus region
    Summary:
    In on_focus_region function,
    entry returns relative x,y position from edje object.
    It should be calculated from elm object's position.
    
    @fix
    
    Test Plan:
    1. $elementary_test "Entry on Page Scroll"
    2. click under button
    3. click upper text in entry
    4. page should not be scrolled
    
    Reviewers: tasn, cedric, woohyun, Hermet, herdsman, raster
    
    Subscribers: Blackmole, id213sin, cedric, jpeg
    
    Differential Revision: https://phab.enlightenment.org/D3923
    
    Change-Id: Ie80a28397d3b4efce1960de1e6b007db205d6c6a
    Signed-off-by: Jiwon Kim <jiwon177.kim@samsung.com>

--- src/lib/elm_entry.c
+++ src/lib/elm_entry.c
@@ -2018,8 +2018,18 @@ _elm_entry_elm_widget_on_focus(Eo *obj, Elm_Entry_Data *sd, Elm_Object_Item *ite
 EOLIAN static Eina_Bool
 _elm_entry_elm_widget_on_focus_region(Eo *obj EINA_UNUSED, Elm_Entry_Data *sd, Evas_Coord *x, Evas_Coord *y, Evas_Coord *w, Evas_Coord *h)
 {
+   Evas_Coord edje_x, edje_y, elm_x, elm_y;
+
    edje_object_part_text_cursor_geometry_get
      (sd->entry_edje, "elm.text", x, y, w, h);
+
+   evas_object_geometry_get(sd->entry_edje, &edje_x, &edje_y, NULL, NULL);
+
+   evas_object_geometry_get(obj, &elm_x, &elm_y, NULL, NULL);
+
+   if (x) *x += edje_x - elm_x;
+   if (y) *y += edje_y - elm_y;
+
    return EINA_TRUE;
 }
 
