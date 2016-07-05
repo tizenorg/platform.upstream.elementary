6305da8fe47b2d0f004f122882f0aaf3eb0c8c97
Sungtaek Hong <sth253.hong@samsung.com>
2016-07-05 16:43:56 +0900

    [Colorselector] add tizen_only colorselector mode which will be deprecated
     - ELM_COLORSELECTOR_PLANE,ELM_COLORSELECTOR_PALETTE_PLANE is added
       to fix ABI break between previous Tizen Versions.
     - These will be deprecated in later version.
    
    Change-Id: I8c3dca03c5df11dea0579ef8e8cee345801da8af
    Signed-off-by: Sungtaek Hong <sth253.hong@samsung.com>

--- src/lib/elm_colorselector_eo.h
+++ src/lib/elm_colorselector_eo.h
@@ -1,1 +1,7 @@
 #include "elm_colorselector.eo.h"
+
+#ifdef gcc
+typedef __attribute__ ((deprecated))Elm_Colorselector_Mode Elm_Colorselector_Mode_Deprecated;
+#define ELM_COLORSELECTOR_PLANE ((Elm_Colorselector_Mode_Deprecated) ELM_COLORSELECTOR_PLANE)
+#define ELM_COLORSELECTOR_PALETTE_PLANE ((Elm_Colorselector_Mode_Deprecated) ELM_COLORSELECTOR_PALETTE_PLANE)
+#endif
